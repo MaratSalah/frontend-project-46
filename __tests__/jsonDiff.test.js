1e4dc5a99a4112f49f66c9d981354451c588d665
Marr <marat.salah1313@gmail.com>
2023-05-12 13:51:26 +0200

    update tests and fixtures

--- __tests__/jsonDiff.test.js
+++ __tests__/jsonDiff.test.js
@@ -2,15 +2,15 @@ import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import path from 'node:path';
-import diff from '../src/diff.js';
+import jsonDiff from '../src/jsonDiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getPathOfFile = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('diff', () => {
-  expect(diff(getPathOfFile('file1.json'), getPathOfFile('file2.json'))).toBe(`{
+test('jsonDiff', () => {
+  expect(jsonDiff(getPathOfFile('file1.json'), getPathOfFile('file2.json'))).toBe(`{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
---
+++ src/ymlDiff.js
@@ -0,0 +1,8 @@
+import fs from 'node:fs';
+import _ from 'lodash';
+import path from 'node:path';
+import yaml from 'js-yaml';
+
+const ymlDiff = (filepath1, filepath2) => {
+
+};
