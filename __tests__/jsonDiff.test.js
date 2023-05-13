7fe99c01b02f16877a3d37ec4842f9b403c9fea5
Marr <marat.salah1313@gmail.com>
2023-05-13 21:24:30 +0200

    update tests and fixtures for nested structures

--- __tests__/jsonDiff.test.js
+++ __tests__/jsonDiff.test.js
@@ -10,12 +10,48 @@ const __dirname = dirname(__filename);
 const getPathOfFile = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('jsonDiff', () => {
-  expect(jsonDiff(getPathOfFile('file1.json'), getPathOfFile('file2.json'))).toBe(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
+  expect(jsonDiff(getPathOfFile('file11.json'), getPathOfFile('file22.json'))).toBe(`{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
 }`);
 });
